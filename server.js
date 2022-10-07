16246a0932fe83a454d0753de50aa4da89f7c6b8
Leon Erasmus <leondevcode@gmail.com>
2022-10-07 20:57:36 -0300

    Mongo Atlas Connection

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 
 const PORT = process.env.PORT || 3000
+const MONGO_URI = process.env.MONGO_URI
 
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(`${MONGO_URI}`, { useNewUrlParser: true })
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Error when trying to connect to the database"))
 
 app.use('/api', routes);
 
