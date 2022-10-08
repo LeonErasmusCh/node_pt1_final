47dfde3839e2b6b8dd7b8e728bd7f38f93a4a3f6
Paulina Godoy <50746324+pag0dy@users.noreply.github.com>
2022-10-08 11:10:12 -0400

    agrega ruta para crear usuario

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user")
+
 
 const hello = async (req, res) => {
     return res.status(200).json({
@@ -5,7 +7,30 @@ const hello = async (req, res) => {
     })
 }
 
+const createHere = async (req, res) => {
+    return res.status(200).json({
+        message: 'This route is ok!'
+    })
+}
+
+const createUser = async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    const newUser = await User.create({ username, email, password });
+
+    const user = await newUser.save()
+
+    return res.status(201).json({
+        status: "Success!",
+        message: 'User created',
+        data: user
+    })
+}
+
 
 module.exports = {
-hello
+hello,
+ createUser,
+ createHere
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const { hello } = require('../controllers/main.controller')
+const { hello, createUser, createHere } = require('../controllers/main.controller')
 const router = require('express').Router();
 
 // Route can be tested at: http://localhost:8080/api/
-router.get('/', hello)            
+router.get('/', hello)    
+router.get('/user/create', createHere)
+router.post('/user/create', createUser)        
            
 
 module.exports = router;
