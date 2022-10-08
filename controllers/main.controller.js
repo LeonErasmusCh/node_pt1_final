1903da0b4da340a605b15e42b21ef889d3a07aca
Leon Erasmus <leondevcode@gmail.com>
2022-10-07 21:24:27 -0300

    user/update

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,11 +1,27 @@
+const User = require('../models/user')
+
 
 const hello = async (req, res) => {
-    return res.status(200).json({
-        message: 'Hello World!'
-    })
-}
+  return res.status(200).json({
+    message: "Hello World!",
+  });
+};
 
+
+const userUpdate = async (req, res) => {
+    const { id } = req.params;
+    const { username, email, isActive, contacts } = req.body;
+
+    await User.findByIdAndUpdate(id, { username, email, isActive, contacts })
+  
+  return res.status(200).json({
+    status: "Success",
+    message: "Hello World!",
+    data: null
+  });
+};
 
 module.exports = {
-hello
-}
+  hello,
+  userUpdate,
+};
