0900905aacfc2c7357495d25828589a9a24da1c8
Leon Erasmus <leondevcode@gmail.com>
2022-10-08 18:42:23 -0300

    add-contact endpoint

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,5 +1,4 @@
-const User = require("../models/user")
-
+const User = require("../models/user");
 
 const hello = async (req, res) => {
   return res.status(200).json({
@@ -8,35 +7,48 @@ const hello = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
+  const { username, email, password } = req.body;
 
-    const { username, email, password } = req.body;
+  const newUser = await User.create({ username, email, password });
 
-    const newUser = await User.create({ username, email, password });
+  const user = await newUser.save();
 
-    const user = await newUser.save()
-
-    return res.status(201).json({
-        status: "Success!",
-        message: 'User created',
-        data: user
-    })
-}
-
-const updateUser = async (req, res) => {
-    const { id } = req.params;
-    const { username, email, isActive, contacts } = req.body;
-    await User.findByIdAndUpdate(id, { username, email, isActive, contacts })
-  
-  return res.status(200).json({
-    status: "Success",
-    message: "User updated",
-    data: {username, email, isActive, contacts}
+  return res.status(201).json({
+    status: "Success!",
+    message: "User created",
+    data: user,
   });
 };
 
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { username, email, isActive, contacts } = req.body;
+  await User.findByIdAndUpdate(id, { username, email, isActive, contacts });
+
+  return res.status(200).json({
+    status: "Success",
+    message: "User updated",
+    data: { username, email, isActive, contacts },
+  });
+};
+
+const addContact = async (req, res) => {
+  const { id } = req.params;
+  const { contact_id } = req.body;
+
+  const newContact = await User.findByIdAndUpdate(id, { contacts: [contact_id] });
+  const updatedContact = await newContact.save();
+
+  return res.status(200).json({
+    status: "Success",
+    message: "Contact added successfully",
+    data: null,
+  });
+};
 
 module.exports = {
   hello,
   createUser,
   updateUser,
+  addContact,
 };
