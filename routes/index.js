696ea9b1fbe2ca0e40e705597e0c6139a7a99c18
Leon Erasmus <leondevcode@gmail.com>
2022-10-08 20:47:18 -0300

    fix to addContact

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const router = require('express').Router();
 router.get('/', hello)
 router.post('/user/create', createUser)            
 router.put('/user/update/:id', updateUser)  // PUT http://localhost:8080/api/user/update/{id}
-router.put('/user/:id/add-contact', addContact) // PUT http://localhost:8080/api/user/{id}/add-contact      
+router.put('/user/:id/create-contact', addContact) // PUT http://localhost:8080/api/user/{id}/create-contact  req.body {"contact_id" : "6341f0125a663d9b073d3e04"}   
 
 module.exports = router;
