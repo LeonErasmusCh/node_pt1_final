a6622feee70893bb6279bb1f8b803f19cefb9e17
Paulina Godoy <50746324+pag0dy@users.noreply.github.com>
2022-10-09 16:15:24 -0400

    modifica lógica de creación de usuarios y agrega login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -32,37 +31,5 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
-UserSchema.pre("save", function (next) {
-  const user = this;
-
-  if (this.isModified("password") || this.isNew) {
-    bcrypt.genSalt(10, function (saltError, salt) {
-      if (saltError) {
-        return next(saltError);
-      } else {
-        bcrypt.hash(user.password, salt, function (hashError, hash) {
-          if (hashError) {
-            return next(hashError);
-          }
-          user.password = hash;
-          next();
-        });
-      }
-    });
-  } else {
-    return next();
-  }
-});
-
-UserSchema.methods.comparePassword = function(password, callback) {
-    bcrypt.compare(password, this.password, function(error, isMatch) {
-        if (error) {
-            return callback(error)
-        } else {
-            callback(null, isMatch)
-        }
-    })
-}
 
 module.exports = mongoose.model("User", UserSchema);
